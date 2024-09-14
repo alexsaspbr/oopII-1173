8ea3927c7a63eb18a8185001480a96f538d8d6e1
Priscila Santos <pri.05@hotmail.com>
2024-09-13 23:42:56 -0300

    adicionando exercicio aula2/pilha resolvido

---
+++ src/aulas/aula2/PilhaImpl.java
@@ -0,0 +1,54 @@
+package aula2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PilhaImpl<E> implements Pilha<E> {
+
+    private final List<E> pilha;
+
+    public PilhaImpl() {
+        this.pilha = new ArrayList<E>();
+    }
+
+    @Override
+    public void push(E elemento) {
+        pilha.add(elemento);
+    }
+
+    @Override
+    public E pop() {
+        try {
+            if(pilha.isEmpty()){
+                throw new RuntimeException("Pilha já está vazia.");
+            }
+            return pilha.remove(pilha.size() - 1);
+        } catch (RuntimeException e) {
+            System.out.println("Pilha já está vazia. Não tem nada para remover");
+            return null;
+        }
+    }
+
+    @Override
+    public E peek() throws Exception {
+        try {
+            if (pilha.isEmpty()) {
+                throw new RuntimeException("A pilha está vazia");
+            }
+            return pilha.get(pilha.size() - 1);
+        } catch (RuntimeException e) {
+            System.out.println("Pilha está vazia. Não tem nada para ver.");
+            return null;
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.pilha.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.pilha.isEmpty();
+    }
+}
