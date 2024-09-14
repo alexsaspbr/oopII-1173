89a1e35440622f4adfcf0311d435db9e2101d8da
Matheus Gomes <matheusbgomes@hotmail.com>
2024-09-13 22:23:20 -0300

    Matheus Gomes Stack

--- src/aulas/App.java
+++ src/aulas/App.java
@@ -1,10 +1,4 @@
-import bancodados.OracleDB;
 import interfaces.Email;
-import interfaces.Persistence;
-import interfaces.Pilha;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class App {
 
@@ -24,14 +18,81 @@ public class App {
 //        Pilha<Integer> pilhaInteiros = new PilhaImpl<>();
 //        pilhaInteiros.push(1);
 
-        Email<PDF> email = new Outlook();
-        envioEmail(email, "mensagem");
+//        Email<PDF> email = new Outlook();
+//        envioEmail(email, "mensagem");
+//
+//
+//    }
+//
+//    public static void envioEmail(Email<PDF> email, String mensagem) {
+//         email.enviarMensagem(mensagem, new PDF());
+//    }
 
+    Delivery pizza = new Delivery();
 
+    Boolean entregasVazio = pizza.isEmpty();
+        if(entregasVazio){
+        System.out.println("Não existem entregas a serem feitas.");
+    } else{
+        System.out.println("Existem entregas a serem feitas!");
     }
 
-    public static void envioEmail(Email<PDF> email, String mensagem) {
-         email.enviarMensagem(mensagem, new PDF());
+        pizza.push("Pizza Calabresa");
+
+        pizza.exibirDelivery();
+
+    entregasVazio = pizza.isEmpty();
+        if(entregasVazio){
+        System.out.println("Não existem entregas a serem feitas.");
+    } else{
+        System.out.println("Existem entregas a serem feitas!");
     }
 
+    String qtdEntregas = String.valueOf(pizza.size());
+        System.out.println("Existe " + qtdEntregas + " entregas a serem feitas.");
+
+        pizza.push("Pizza 4 Queijos");
+
+        pizza.exibirDelivery();
+
+    qtdEntregas = String.valueOf(pizza.size());
+        System.out.println("Existe " + qtdEntregas + " entregas a serem feitas.");
+
+        try{
+        String primeiraEntrega = pizza.peek();
+        System.out.println("A primeira entrega é: " + primeiraEntrega);
+    } catch (Exception e){
+        System.out.println(e.getMessage());
+    }
+
+    String entregaRemovida = pizza.pop();
+        System.out.println("Uma entrega foi removida. Entrega atual: " + entregaRemovida);
+
+        try{
+        String primeiraEntrega = pizza.peek();
+        System.out.println("A primeira entrega é: " + primeiraEntrega);
+    } catch (Exception e){
+        System.out.println(e.getMessage());
+    }
+
+        pizza.exibirDelivery();
+
+    entregasVazio = pizza.isEmpty();
+        if(entregasVazio){
+        System.out.println("Não existem entregas a serem feitas.");
+    } else{
+        System.out.println("Existem entregas a serem feitas!");
+    }
+
+    entregaRemovida = pizza.pop();
+        System.out.println("Uma entrega foi removida. Entrega atual: " + entregaRemovida);
+
+        try{
+        String primeiraEntrega = pizza.peek();
+        System.out.println("A primeira entrega é: " + primeiraEntrega);
+    } catch (Exception e){
+        System.err.println(e.getMessage());
+    }
+
+}
 }
