f2ea4c9071bb0c53034cae0a66925559ee9a6f7a
Alex Araujo <alexsaspbr@gmail.com>
2024-09-25 21:10:10 -0300

    modelos de aluguel e devolucao

---
+++ src/java/tech/ada/locadora/modelo/aluguel/Aluguel.java
@@ -0,0 +1,56 @@
+package tech.ada.locadora.modelo.aluguel;
+
+import tech.ada.locadora.modelo.agencia.Agencia;
+import tech.ada.locadora.modelo.pessoa.Pessoa;
+import tech.ada.locadora.modelo.veiculo.Veiculo;
+
+import java.time.LocalDateTime;
+
+public class Aluguel {
+
+    protected Pessoa cliente;
+    protected Agencia agencia;
+    protected Veiculo veiculo;
+    protected LocalDateTime dataInicio;
+    protected LocalDateTime dataDevolucaoPrevista;
+
+    public Pessoa getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Pessoa cliente) {
+        this.cliente = cliente;
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Agencia agencia) {
+        this.agencia = agencia;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public LocalDateTime getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDateTime dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDateTime getDataDevolucaoPrevista() {
+        return dataDevolucaoPrevista;
+    }
+
+    public void setDataDevolucaoPrevista(LocalDateTime dataDevolucaoPrevista) {
+        this.dataDevolucaoPrevista = dataDevolucaoPrevista;
+    }
+}
---
+++ src/java/tech/ada/locadora/modelo/aluguel/Devolucao.java
@@ -0,0 +1,9 @@
+package tech.ada.locadora.modelo.aluguel;
+
+import java.time.LocalDateTime;
+
+public class Devolucao extends Aluguel {
+
+    private LocalDateTime getDataDevolucaoFinal;
+
+}
